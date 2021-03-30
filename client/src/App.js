25da4e28b2927a12486e1295e310d11e048c5e5b
OrHayat <hayatb@post.bgu.ac.il>
2021-03-30 21:34:24 +0300

    added route to home

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import Header from "./components/header/Header";
 import Navbar from "./components/navbar/Navbar";
 import Search from "./components/search/Search";
 import AddRecommendation from "./components/addRecommendation/AddRecommendation";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route,Switch } from "react-router-dom";
 import { GlobalStyles } from "./styles/global";
 
 function App() {
@@ -12,15 +12,19 @@ function App() {
       <GlobalStyles />
       <Navbar />
       <Header />
-      <Route path="/"></Route>
-      <Search />
-      <AddRecommendation
-        props={{
-          title: "Night",
-          searchType: "book",
-          creator: "Roman Polanski",
-        }}
-      />
+      <Switch>
+          <Route path="/" exact >foooooooooo</Route>
+          <Route>
+              <Search />
+              <AddRecommendation
+                  props={{
+                      title: "Night",
+                      searchType: "book",
+                      creator: "Roman Polanski",
+                  }}
+              />
+          </Route>
+      </Switch>
     </Router>
   );
 }
