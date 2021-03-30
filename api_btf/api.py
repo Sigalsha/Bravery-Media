02d37c1d11806cddec6a4a10be107de0162f98a5
gtaya <gelmantaya@gmail.com>
2021-03-30 19:35:40 +0300

    updated post requests

--- api_btf/api.py
+++ api_btf/api.py
@@ -1,7 +1,6 @@
 from flask import Flask, request
 from data_manager.Queries import *
 import json
-import os
 
 
 app = Flask(__name__)
@@ -10,32 +9,41 @@ app = Flask(__name__)
 @app.route('/get_top_items', methods=['GET'])
 def get_top_items():
     category = request.args.get("category")
-    search_favorites_by_category(category)
+    top_items = search_favorites_by_category(category)
+    return json.dumps(top_items)
 
 
 @app.route('/get_search_results', methods=['GET'])
-def navigate_to_search_results():
+def get_search_results():
     item_type = request.args.get("item_type")
     keywords = request.args.get("keywords")
-    search_by_type(item_type, keywords)
+    search_results = search_by_type(item_type, keywords)
+    return json.dumps(search_results)
 
 
 @app.route('/get_item', methods=['GET'])
 def get_item():
     item_id = request.args.get("item_id")
-    get_item_info(item_id)
+    item_info = get_item_info(item_id)
+    return json.dumps(item_info)
+
 
 @app.route('/add_recommendation', methods=['POST'])
-def add_review():
-    review_json = request.json
-    item_id = review_json.id
-    bravery_moments = review_json.selectedHeroismMoments
-    content = review_json.recommendation
-    reviewer = review_json.recommenderName
+def add_recommendation():
+    recommendation = request.json
+    item_id = recommendation.id
+    bravery_moments = recommendation.selectedHeroismMoments
+    content = recommendation.recommendation
+    reviewer = recommendation.recommenderName
+    add_review(item_id, bravery_moments, content, reviewer)
 
 
-@app.route('/add_rating', methods=['POST'])
-def add_rating():
+@app.route('/add_heroism_rate', methods=['POST'])
+def add_heroism_rate():
+    rating = request.json
+    item_id = rating.id
+    item_rating = rating.heroism_rate
+    add_rating(item_id, item_rating)
 
 
 if __name__ == '__main__':
