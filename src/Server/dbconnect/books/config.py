0789606be95590e043a09fdfc73c69c24734b03f
gtaya <gelmantaya@gmail.com>
2021-03-31 14:00:54 +0300

    added google books conn

---
+++ src/Server/dbconnect/books/config.py
@@ -0,0 +1,1 @@
+google_api_key = "AIzaSyAEfzq99KQOFj_LLuo0SIojpg6ub8QniYw"
--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from Server.dbconnect.repository import repo
 from Server.dbconnect.daos import *
-from Server.dbconnect import imdb_conn
+from Server.dbconnect.books import google_books_conn as gbc
 
 if __name__ == '__main__':
-         repo.create_tables()
+    repo.create_tables()
+    gbc.search_by_keyword("polanski")
